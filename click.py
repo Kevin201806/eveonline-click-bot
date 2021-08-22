f46eccb14e89c4675e74a0bba12fc5419b465a36
LianQi <40719108+Kevin201806@users.noreply.github.com>
2021-08-23 03:39:39 +0800

    a little updata

--- click.py
+++ click.py
@@ -1,23 +1,28 @@
 import codecs
 import os
+import re
+import string
 import time
-import playsound
 import pyautogui
+import numpy as np
+from some_dict import glaxy_dict
 
 
 # pyautogui样例
 # https://blog.csdn.net/weixin_43430036/article/details/84650938
-def write_and_click(x1, y1, write):
+def write_and_click(x1, y1, write, speed):
     # 点击两次 绝对坐标
     pyautogui.click(x1, y1)
     pyautogui.click(x1, y1)
-    # 输入"write"的内容 花费时间0.7s
-    pyautogui.typewrite(message=str(write), interval=0.7)
+    # 输入"write"的内容 花费时间speed秒
+    pyautogui.typewrite(message=str(write), interval=speed)
     # 按下/抬起enter键
+    time.sleep(0.2)
     pyautogui.keyDown('enter')
     pyautogui.keyUp('enter')
     # 打印成功信息
     print("Seccess write " + str(write) + " on " + str(x1) + " " + str(y1))
+
 
 # 从日志文件夹内取出最新的日志文件
 def getname(path, specword):
@@ -31,15 +36,23 @@ def getname(path, specword):
     else:
         return namelist[-1]
 
-# 判断字符串是否包含中文
-# def check_contain_chinese(check_str):
-#     for ch in check_str.decode('utf-8'):
-#         if u'\u4e00' <= ch <= u'\u9fff':
-#             return True
-#     return False
+
+# 判断是否为英文 是True 否False
+def is_english_char(check_str):
+    # print(check_str)
+    punc = string.punctuation
+    zhPattern = re.compile(u'[\u4e00-\u9fa5]+')
+    for ch in check_str:
+        if ch not in punc:
+            match = zhPattern.search(ch)
+            if match:
+                return False
+            else:
+                return True
+
 
 # 从读取日志并匹配最后一行
-def take_log(Chatname, spec_word_list, logpath, Mask_list):
+def take_log(Chatname, logpath):
     # 本地时间
     # today = str(datetime.date.today()).split("-")
     # 格林威治时间
@@ -49,38 +62,143 @@ def take_log(Chatname, spec_word_list, logpath, Mask_list):
         last_line = f.readlines()[-1]  # 取最后一行
         # print('最后一行为：' + last_line)
     talk_th = last_line.split(' > ')[1][0:-2].split(" ")
+    return talk_th
+
+
+# 匹配
+def check_spec_word(talk_th, spec_word_list, Mask_list):
+    # print(talk_th)
     # if check_contain_chinese(talk_th) == False:
-    for word in spec_word_list:
-        if word in talk_th:
-            typeA = True
-            for word2 in Mask_list:
-                for word3 in talk_th:
-                    if word2 in word3:
-                        typeA = False
-            if typeA == True:
-                print(talk_th)
-                return "click"
+    AAlist = []
+    for A in talk_th:
+        if A != '' or A != ' ':
+            AAlist.append(A)
+    # chTYPE = False
+    # for a in AAlist:
+    #     if is_english_char(a) == False:
+    #         # print(str(a) + " is chinese")
+    #         chTYPE = True
+    #         break
+    # print(chTYPE)
+    typeB = False
+    for AAAA in AAlist:
+        if AAAA in spec_word_list:
+            typeB = True
+            # print(talk_th)
+            break
+    # print(chTYPE)
+    # print(typeB)
+    # if typeB == True and chTYPE == False:
+    if typeB == True:
+        print("含有关键词, 开始匹配: " + str(talk_th))
+        galaxy = ""
+        typeYZ = ""
+        much = ""
+        hour = ""
+        for thi in AAlist:
+            if thi in spec_word_list:
+                typeYZ = thi
+            elif is_english_char(thi) == True:
+                # print(thi)
+                if thi[-1] in punc:
+                    thi = thi[0:-1]
+                if thi in final_list1:
+                    galaxy = thi
+                elif len(thi) == 3 and thi[-1] in ["h", "H"]:
+                    hour = thi
+                elif len(thi) == 4 and thi[-2] in ["h", "H"]:
+                    hour = thi
+                elif len(thi) == 4 and thi[-1] in ["M", "m"]:
+                    much = thi
+                elif len(thi) == 3 and thi in ["200", "130", "140", "150", "210"]:
+                    much = thi
+                elif len(thi) == 7 and thi[3] in ["m","M"]:
+                    much = thi
+                elif len(thi) == 6 and thi[2] in ["H","h"] and thi[5] in ["M","m"]:
+                    thi = time
+        final_list = [galaxy, hour, much, typeYZ]
+        print("匹配结果: " + str(final_list))
+        return final_list
+
 
 if __name__ == '__main__':
-
+    punc = string.punctuation
     # 需要匹配的关键词
-    # spec_word_list = ["6/10", "10/10", "6-10", "10-10", "1010"]
-    spec_word_list = ["10/10", "10-10","1010"]
+    spec_word_list = ["6/10", "10/10", "6-10", "10-10", "1010","10","maze","Maze"]
+    # spec_word_list = ["10/10", "10-10","1010"]
     # EVE频道日志的绝对位置
     logpath = "c://users//18201//Documents//EVE//logs//Chatlogs//"
     # 如果存在列表内的词则忽略
-    mask_list = ["置顶","野生","预定","测试","维纳尔","自己","工具","签约","找个","test","有人","出吗","脚本","出么"]
+    Mask_list = ["置顶", "野生", "预定", "测试", "维纳尔", "自己", "工具", "签约", "找个", "test", "有人", "出吗", "脚本", "出么", "野生"]
     # 频道名称
     chatname = "Escalation 静寂谷"
+    ## 星系列表
+    # 特布特 Tribute
+    Tribute_list_ss = glaxy_dict["Tribute"]["glaxy_dict"]["ss"]
+    Tribute_list_es = glaxy_dict["Tribute"]["glaxy_dict"]["es"]
+    # 维纳尔 Venal
+    Venal_list_ss = glaxy_dict["Venal"]["glaxy_dict"]["ss"]
+    Venal_list_es = glaxy_dict["Venal"]["glaxy_dict"]["es"]
+    # 寂静谷 Vale_of_the_Silent
+    Vale_of_the_Silent_list_ss = glaxy_dict["Vale_of_the_Silent"]["glaxy_dict"]["ss"]
+    Vale_of_the_Silent_list_es = glaxy_dict["Vale_of_the_Silent"]["glaxy_dict"]["es"]
+    # 对舞之域 Geminate
+    Geminate_list_ss = glaxy_dict["Geminate"]["glaxy_dict"]["ss"]
+    Geminate_list_es = glaxy_dict["Geminate"]["glaxy_dict"]["es"]
+    # 拼接列表并去重 寂静谷+维纳尔+特布特+对舞
+    final_list1 = list(set(Tribute_list_ss + Tribute_list_es +
+                        Vale_of_the_Silent_list_ss + Vale_of_the_Silent_list_es + \
+                        Venal_list_es + Venal_list_ss + \
+                        Geminate_list_ss + Geminate_list_es))
+    # 拼接列表并去重 寂静谷 + 特布特
+    final_list2 = list(set(Tribute_list_ss + Tribute_list_es +
+                           Vale_of_the_Silent_list_ss + Vale_of_the_Silent_list_es))
     while True:
-        AAA = take_log(chatname, spec_word_list, logpath, mask_list)
-        if AAA == "click":
-            # 在屏幕的绝对坐标输入1
-            write_and_click(390, 593, "1")
-            break
-        # time.sleep(0.2)
-    # 检测到则播放音频 可以选择wav/mp3格式的
-    playsound.playsound('hp.wav')
+        gmtimes = time.strftime("%Y-%m-%d/%H:%M:%S", time.gmtime(time.time()))
+        talk_th = take_log(chatname, logpath)
+        final_list = check_spec_word(talk_th, spec_word_list, Mask_list)
+        if final_list != None:
+            # print(final_list)
+            galaxy, hour, much, typeYZ = final_list[0], final_list[1], final_list[2], final_list[3]
+            default_choice = True
+            if hour != "":
+                if int(hour[0:2]) <= 12 or int(hour[0:2]) > 24:
+                    default_choice = False
+            if much != "":
+                if int(much[0:3]) < 60 or int(much[0:3]) > 240:
+                    # print(much[0:3])
+                    default_choice = False
+            # if galaxy not in final_list1:
+            #     default_choice = False
+            num0 = 0
+            for sth in final_list:
+                if sth != "":
+                    num0 += 1
+            # print(default_choice)
+            rand_num = float(str(np.random.uniform(0.5,1))[0:3])
+            if default_choice == True and num0 >= 2:
+                print("条件成立，判定是否为指定远征: " + str(final_list))
+                if typeYZ != "" and galaxy != "":
+                    if "6" not in final_list[3]:
+                        print("是10/10，开始点击")
+                        write_and_click(2303, 611, "1", rand_num)
+                        write_and_click(617, 612, "1", rand_num)
+                        time.sleep(5)
+                        os.system("hp.wav")
+                # else:
+                #     write_and_click(373, 609, "1", rand_num)
+                #     time.sleep(5)
+                #     os.system("hp.wav")
+                # write_and_click(394, 609, "1", 0.8)
+                with open("point_his.txt", "a") as point_his:
+                    point_his.write(str(gmtimes) + "\n")
+                    point_his.write(str(final_list) + "\n")
+                    point_his.close()
+                # print(final_list)
+                # os.system("hp.wav")
+                time.sleep(10)
+                # break
+            # break
 
 # 测试专用行 用来切换回IDE
 # pyautogui.click(2840,476)
--- glaxy_spider.py
+++ glaxy_spider.py
@@ -1,19 +1,13 @@
 import codecs
 import requests
 from bs4 import BeautifulSoup
+from some_dict import glaxy_dict, url_dict
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
     "Connection": "keep-alive",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "zh-CN,zh;q=0.8"}
-
-
-# def creat_xlsx_file(dict,filename):
-#     wb = Workbook()
-#     ws = wb.active
-#     ws1 = wb.create_sheet('star_field', 0)
-#
 
 def get_glaxy_list(star_field_url, proxies=None, timeout=None, headers=headers):
     # get htmlhandls
@@ -65,75 +59,15 @@ if __name__ == '__main__':
     # url_dict = creat_star_field_dic(map_basic_url)
 
     # 为节省时间,直接将字典写于此
-    url_dict = {'Aridia': {'url': 'https://evemaps.dotlan.net/svg/Aridia.svg'},
-                'Black_Rise': {'url': 'https://evemaps.dotlan.net/svg/Black_Rise.svg'},
-                'The_Bleak_Lands': {'url': 'https://evemaps.dotlan.net/svg/The_Bleak_Lands.svg'},
-                'Branch': {'url': 'https://evemaps.dotlan.net/svg/Branch.svg'},
-                'Cache': {'url': 'https://evemaps.dotlan.net/svg/Cache.svg'},
-                'Catch': {'url': 'https://evemaps.dotlan.net/svg/Catch.svg'},
-                'The_Citadel': {'url': 'https://evemaps.dotlan.net/svg/The_Citadel.svg'},
-                'Cloud_Ring': {'url': 'https://evemaps.dotlan.net/svg/Cloud_Ring.svg'},
-                'Cobalt_Edge': {'url': 'https://evemaps.dotlan.net/svg/Cobalt_Edge.svg'},
-                'Curse': {'url': 'https://evemaps.dotlan.net/svg/Curse.svg'},
-                'Deklein': {'url': 'https://evemaps.dotlan.net/svg/Deklein.svg'},
-                'Delve': {'url': 'https://evemaps.dotlan.net/svg/Delve.svg'},
-                'Derelik': {'url': 'https://evemaps.dotlan.net/svg/Derelik.svg'},
-                'Detorid': {'url': 'https://evemaps.dotlan.net/svg/Detorid.svg'},
-                'Devoid': {'url': 'https://evemaps.dotlan.net/svg/Devoid.svg'},
-                'Domain': {'url': 'https://evemaps.dotlan.net/svg/Domain.svg'},
-                'Esoteria': {'url': 'https://evemaps.dotlan.net/svg/Esoteria.svg'},
-                'Essence': {'url': 'https://evemaps.dotlan.net/svg/Essence.svg'},
-                'Etherium_Reach': {'url': 'https://evemaps.dotlan.net/svg/Etherium_Reach.svg'},
-                'Everyshore': {'url': 'https://evemaps.dotlan.net/svg/Everyshore.svg'},
-                'Fade': {'url': 'https://evemaps.dotlan.net/svg/Fade.svg'},
-                'Feythabolis': {'url': 'https://evemaps.dotlan.net/svg/Feythabolis.svg'},
-                'The_Forge': {'url': 'https://evemaps.dotlan.net/svg/The_Forge.svg'},
-                'Fountain': {'url': 'https://evemaps.dotlan.net/svg/Fountain.svg'},
-                'Geminate': {'url': 'https://evemaps.dotlan.net/svg/Geminate.svg'},
-                'Genesis': {'url': 'https://evemaps.dotlan.net/svg/Genesis.svg'},
-                'Great_Wildlands': {'url': 'https://evemaps.dotlan.net/svg/Great_Wildlands.svg'},
-                'Heimatar': {'url': 'https://evemaps.dotlan.net/svg/Heimatar.svg'},
-                'Immensea': {'url': 'https://evemaps.dotlan.net/svg/Immensea.svg'},
-                'Impass': {'url': 'https://evemaps.dotlan.net/svg/Impass.svg'},
-                'Insmother': {'url': 'https://evemaps.dotlan.net/svg/Insmother.svg'},
-                'Kador': {'url': 'https://evemaps.dotlan.net/svg/Kador.svg'},
-                'The_Kalevala_Expanse': {'url': 'https://evemaps.dotlan.net/svg/The_Kalevala_Expanse.svg'},
-                'Khanid': {'url': 'https://evemaps.dotlan.net/svg/Khanid.svg'},
-                'Kor-Azor': {'url': 'https://evemaps.dotlan.net/svg/Kor-Azor.svg'},
-                'Lonetrek': {'url': 'https://evemaps.dotlan.net/svg/Lonetrek.svg'},
-                'Malpais': {'url': 'https://evemaps.dotlan.net/svg/Malpais.svg'},
-                'Metropolis': {'url': 'https://evemaps.dotlan.net/svg/Metropolis.svg'},
-                'Molden_Heath': {'url': 'https://evemaps.dotlan.net/svg/Molden_Heath.svg'},
-                'Oasa': {'url': 'https://evemaps.dotlan.net/svg/Oasa.svg'},
-                'Omist': {'url': 'https://evemaps.dotlan.net/svg/Omist.svg'},
-                'Outer_Passage': {'url': 'https://evemaps.dotlan.net/svg/Outer_Passage.svg'},
-                'Outer_Ring': {'url': 'https://evemaps.dotlan.net/svg/Outer_Ring.svg'},
-                'Paragon_Soul': {'url': 'https://evemaps.dotlan.net/svg/Paragon_Soul.svg'},
-                'Period_Basis': {'url': 'https://evemaps.dotlan.net/svg/Period_Basis.svg'},
-                'Perrigen_Falls': {'url': 'https://evemaps.dotlan.net/svg/Perrigen_Falls.svg'},
-                'Placid': {'url': 'https://evemaps.dotlan.net/svg/Placid.svg'},
-                'Pochven': {'url': 'https://evemaps.dotlan.net/svg/Pochven.svg'},
-                'Providence': {'url': 'https://evemaps.dotlan.net/svg/Providence.svg'},
-                'Pure_Blind': {'url': 'https://evemaps.dotlan.net/svg/Pure_Blind.svg'},
-                'Querious': {'url': 'https://evemaps.dotlan.net/svg/Querious.svg'},
-                'Scalding_Pass': {'url': 'https://evemaps.dotlan.net/svg/Scalding_Pass.svg'},
-                'Sinq_Laison': {'url': 'https://evemaps.dotlan.net/svg/Sinq_Laison.svg'},
-                'Solitude': {'url': 'https://evemaps.dotlan.net/svg/Solitude.svg'},
-                'The_Spire': {'url': 'https://evemaps.dotlan.net/svg/The_Spire.svg'},
-                'Stain': {'url': 'https://evemaps.dotlan.net/svg/Stain.svg'},
-                'Syndicate': {'url': 'https://evemaps.dotlan.net/svg/Syndicate.svg'},
-                'Tash-Murkon': {'url': 'https://evemaps.dotlan.net/svg/Tash-Murkon.svg'},
-                'Tenal': {'url': 'https://evemaps.dotlan.net/svg/Tenal.svg'},
-                'Tenerifis': {'url': 'https://evemaps.dotlan.net/svg/Tenerifis.svg'},
-                'Tribute': {'url': 'https://evemaps.dotlan.net/svg/Tribute.svg'},
-                'Vale_of_the_Silent': {'url': 'https://evemaps.dotlan.net/svg/Vale_of_the_Silent.svg'},
-                'Venal': {'url': 'https://evemaps.dotlan.net/svg/Venal.svg'},
-                'Verge_Vendor': {'url': 'https://evemaps.dotlan.net/svg/Verge_Vendor.svg'},
-                'Wicked_Creek': {'url': 'https://evemaps.dotlan.net/svg/Wicked_Creek.svg'}}
-    for name in url_dict:
-        url = url_dict[name]["url"]
-        print(name + ": " + url)
-        glaxy_dict = get_glaxy_list(url)
-        url_dict[name]["glaxy_dict"] = glaxy_dict
-        # break
-    print(url_dict)
+    url_dict = url_dict
+
+    # 创建星系列表
+    # for name in url_dict:
+    #     url = url_dict[name]["url"]
+    #     print(name + ": " + url)
+    #     glaxy_dict = get_glaxy_list(url)
+    #     url_dict[name]["glaxy_dict"] = glaxy_dict
+    glaxy_dict = glaxy_dict
+
+    print(glaxy_dict)
+
